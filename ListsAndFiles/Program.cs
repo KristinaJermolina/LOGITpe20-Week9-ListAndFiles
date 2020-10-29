381e5355549ee06ac08f1cee8e5efbd988bf8f4e
Opilane TTHK <opilane@tthk.ee>
2020-10-29 11:36:49 +0200

    first commit

---
+++ ListsAndFiles/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListsAndFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> fruit = new List<string>();
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("enter a fruit");
+                fruit.Add(Console.ReadLine());
+            }
+            fruit.Add("pineapple");
+
+            foreach(string fruitItem in fruit)
+            {
+                Console.WriteLine($"fruit on the list: {fruitItem}");
+            }
+
+            int indexOfPineapple = fruit.IndexOf("pineapple");
+
+            Console.WriteLine($"pineapple is on index {indexOfPineapple}");
+
+            fruit[indexOfPineapple] = "orange";
+
+            foreach (string fruitItem in fruit)
+            {
+                Console.WriteLine($"fruit on the updated list: {fruitItem}");
+            }
+
+            fruit.RemoveAt(fruit.Count - 1);
+
+            foreach (string fruitItem in fruit)
+            {
+                Console.WriteLine($"trimmed fruit list: {fruitItem}");
+            }
+
+
+        }
+    }
+}
---
+++ WorkingWithFiles/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WorkingWithFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\opilane\data\test.txt";
+            List<string> dataFromFile = File.ReadAllLines(filePath).ToList();
+            foreach(string line in dataFromFile)
+            {
+                Console.WriteLine($"Data from line : {line}");
+            }
+            dataFromFile.Add("grapes");
+            File.WriteAllLines(filePath, dataFromFile);
+        }
+    }
+}
